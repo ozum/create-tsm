c6f032dbae327e91e3828bfc8da4cc22bee34883
Özüm Eldoğan <ozum@ozum.net>
2024-05-30 12:34:47 +0300

    fix: update knip config

--- config/tsm/base-knip.js
+++ config/tsm/base-knip.js
@@ -1,6 +1,6 @@
 export default {
   ignore: ["src/@types/**", "src/bin/**", ".eslintrc-maximum.cjs"],
-  ignoreDependencies: ["@types/jest", "@jest/globals"],
-  ignoreBinaries: ["audit-ci", "auto"], // auto (base-build-and-test.yml) falsely recognized as a binary.
+  ignoreDependencies: ["@types/jest", "@jest/globals", "audit-ci"],
+  ignoreBinaries: ["auto"], // auto (base-build-and-test.yml) falsely recognized as a binary.
   entry: ["src/index.ts", "src/bin"],
 };
